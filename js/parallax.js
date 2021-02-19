fcfd365c1100f0348e7c57cd0f567978c9557950
dgbracero <diegobracero@hotmail.com>
2021-02-19 14:58:26 -0500

    Parallax del sitio

--- js/parallax.js
+++ js/parallax.js
@@ -1,3 +1,26 @@
 $(document).ready(function(){
-    
+    $(window).scroll(function(){
+        let windowWidth = $(window).width();
+        if(windowWidth > 800){
+            var scroll = $(window).scrollTop();
+            $('header .textos').css({
+                'transform': 'translate(0px, '+ scroll / 3 +'%)'
+            });
+
+            $('.main .article').css({
+                'transform': 'translate(0px, -'+ scroll / 5 +'%)'
+            });
+
+        }
+    });
+
+    $(window).resize(function(){
+        let windowWidth = $(window).width();
+        if(windowWidth < 800){
+            $('.main .article').css({
+                'transform': 'translate(0px, 0px)'
+            });
+        }
+    });
+
 });
